969b3f2ca1e20a07fb6eb06c2a23d8de7e44e76f
Paddy Drury <paddy.e.drury@gmail.com>
2020-12-02 10:38:49 +0000

    tidy up

--- day2/day2.groovy
+++ day2/day2.groovy
@@ -1,41 +1,31 @@
-assert countValidPasswords1("testinput.txt") == 2
-assert countValidPasswords2("testinput.txt") == 1
+#!/usr/bin/env groovy
 
-def count1 = countValidPasswords1("input.txt")
-def count2 = countValidPasswords2("input.txt")
+assert countValidPasswords("testinput.txt", this.&validatePassword1) == 2
+assert countValidPasswords("testinput.txt", this.&validatePassword2) == 1
+
+def count1 = countValidPasswords("input.txt", this.&validatePassword1)
+def count2 = countValidPasswords("input.txt", this.&validatePassword2)
 
 println count1
 println count2
 
-def countValidPasswords1(path) {
-    (path as File).readLines().findAll {checkPassword1(it)}.size()
+
+def countValidPasswords(path, passwordValidator) {
+    (path as File).readLines().findAll {checkPassword(it, passwordValidator)}.size()
 }
 
-def countValidPasswords2(path) {
-    (path as File).readLines().findAll {checkPassword2(it)}.size()
-}
 
-def checkPassword1(String passwordLine) {
 
-    println "Checking password $passwordLine"
-
-    (passwordLine =~ /(?<min>\d+)-(?<max>\d+) (?<letter>[a-z]):\s(?<password>[a-z]+)/).find { match, min, max, letter, password ->
-        def regex = /^([^${letter}]*$letter[^$letter]*){$min,$max}$/
-        def matches = password.matches(regex)
-
-        println "$password ${matches ? "matches" : "doesn't match"} $regex"
-        matches
+def checkPassword(passwordLine, passwordValidator) {
+    (passwordLine =~ /(?<num1>\d+)-(?<num2>\d+) (?<letter>[a-z]):\s(?<password>[a-z]+)/).find { match, num1, num2, letter, password ->
+        passwordValidator(num1, num2, letter, password)
     }
 }
 
-def checkPassword2(String passwordLine) {
+def validatePassword1(min, max, letter, password) {
+    password.matches(/^([^${letter}]*$letter[^$letter]*){$min,$max}$/)
+}
 
-    println "Checking password $passwordLine"
-
-    (passwordLine =~ /(?<min>\d+)-(?<max>\d+) (?<letter>[a-z]):\s(?<password>[a-z]+)/).find { match, pos1, pos2, letter, password ->
-        def matches = (password as String).getAt(pos1.toInteger()-1) == letter ^ (password as String).getAt(pos2.toInteger()-1) == letter
-
-        println "$password ${matches ? "matches" : "doesn't match"}"
-        matches
-    }
+def validatePassword2(pos1, pos2, letter, password) {
+    (password as String).getAt(pos1.toInteger()-1) == letter ^ (password as String).getAt(pos2.toInteger()-1) == letter
 }
