e0e5c196b82bdd0adc3709efea343585424ac056
Paddy Drury <paddy.e.drury@gmail.com>
2020-12-08 15:01:40 +0000

    day 8  fugly

--- kotlin/src/main/kotlin/day8/Day8.kt
+++ kotlin/src/main/kotlin/day8/Day8.kt
@@ -5,42 +5,33 @@ import util.readFile
 class Day8(inputFile: String) {
     private val lines: List<String> = readFile(inputFile)
 
-    fun part1(): Int = lines.toInstructions().executeFrom(0, 0, mutableListOf()).first
+    fun part1(): Int = lines.toInstructions().execute().first
 
     fun part2(): Int {
         val initialInstructions = lines.toInstructions()
-        val initialResult = initialInstructions.executeFrom(0, 0, mutableListOf())
+        val initialResult = initialInstructions.execute()
         initialResult.third
-                .filter { it.first.operation in listOf("jmp", "nop") }
+                .filter { it.first.isJumpOrNop() }
                 .reversed()
-                .fold(0) { _, item ->
+                .forEach { item ->
                     val instruction = item.first
                     val instructions = initialInstructions.toMutableList()
-                    instructions[instruction.line] = Instruction(
-                            instruction.line,
-                            when(instruction.operation) {
-                                "jmp" -> "nop"
-                                "nop" -> "jmp"
-                                else -> error("blah")
-                            }, instruction.argument
-                    )
-
+                    instructions[instruction.line] = instruction.toggled()
                     val result = instructions.executeFrom(instruction.line, item.second, initialResult.third)
                     if(result.second >= instructions.size) {
                         return result.first
                     }
-                    0
                 }
         error("failed to find way out of program")
     }
 
+    fun List<Instruction>.execute() = executeFrom(0, 0, mutableListOf())
 
     fun List<Instruction>.executeFrom(startIndex: Int, startValue: Int, history: MutableList<Pair<Instruction, Int>>): Triple<Int, Int, MutableList<Pair<Instruction, Int>>> {
         var index = startIndex
         var value = startValue
         while(index < this.size && !history.any { it.first === this[index]}) {
             history += Pair(this[index], value)
-            println(this[index])
             when(this[index].operation) {
                 "nop" -> index++
                 "acc" -> value += this[index++].argument
@@ -51,7 +42,18 @@ class Day8(inputFile: String) {
     }
 }
 
-data class Instruction(val line: Int, val operation: String,  val argument: Int)
+data class Instruction(val line: Int, val operation: String,  val argument: Int) {
+    fun toggled(): Instruction = Instruction(
+            this.line,
+            when(this.operation) {
+                "jmp" -> "nop"
+                "nop" -> "jmp"
+                else -> error("blah")
+            }, this.argument
+    )
+
+    fun isJumpOrNop() = this.operation in listOf("jmp", "nop")
+}
 
 fun Iterable<String>.toInstructions(): List<Instruction> = this.mapIndexed { index, line ->
     val(operation, argumentStr) = """^([a-z]+) ([+\-]\d+)${'$'}""".toRegex().matchEntire(line)!!.destructured
