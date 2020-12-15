3022dd7c2ddc181418547592e67f0e729fd73140
Paddy Drury <paddy.e.drury@gmail.com>
2020-12-15 15:05:40 +0000

    tidy a bit

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -1,17 +1,5 @@
 package day15
 
-data class Day15(val numbers: MutableList<Int>) {
-    fun part1(): Int {
-        while(numbers.size < 2020) {
-            numbers.add(nextNumber())
-        }
-        println(numbers)
-        return numbers.last()
-    }
-
-    fun nextNumber(): Int = numbers.dropLast(1).reversed().indexOf(numbers.last()) + 1
-}
-
 data class Day15Map(val startingNumbers: List<Int>) {
     var count = startingNumbers.size
     var nextVal = 0
@@ -33,6 +21,7 @@ data class Day15Map(val startingNumbers: List<Int>) {
         }
         return nextVal
     }
+
 }
 
 fun MutableMap<Int, Int>.nextVal(value: Int, turn: Int): Int {
--- src/test/kotlin/day15/ADay15.kt
+++ src/test/kotlin/day15/ADay15.kt
@@ -6,36 +6,35 @@ import org.junit.Test
 class ADay15 {
     @Test
     fun testPart1132() {
-        assertThat(Day15(mutableListOf(1,3,2)).part1()).isEqualTo(1)
-        assertThat(Day15Map(mutableListOf(1,3,2)).part1()).isEqualTo(1)
+        assertThat(Day15Map(listOf(1,3,2)).part1()).isEqualTo(1)
     }
     @Test
     fun testPart1213() {
-        assertThat(Day15(mutableListOf(2, 1, 3)).part1()).isEqualTo(10)
+        assertThat(Day15Map(listOf(2, 1, 3)).part1()).isEqualTo(10)
     }
     @Test
     fun testPart1123() {
-        assertThat(Day15(mutableListOf(1,2,3)).part1()).isEqualTo(27)
+        assertThat(Day15Map(listOf(1,2,3)).part1()).isEqualTo(27)
     }
     @Test
     fun testPart1231() {
-        assertThat(Day15(mutableListOf(2,3,1)).part1()).isEqualTo(78)
+        assertThat(Day15Map(listOf(2,3,1)).part1()).isEqualTo(78)
     }
     @Test
     fun testPart1321() {
-        assertThat(Day15(mutableListOf(3,2,1)).part1()).isEqualTo(438)
+        assertThat(Day15Map(listOf(3,2,1)).part1()).isEqualTo(438)
     }
     @Test
     fun testPart1312() {
-        assertThat(Day15(mutableListOf(3, 1, 2)).part1()).isEqualTo(1836)
+        assertThat(Day15Map(listOf(3, 1, 2)).part1()).isEqualTo(1836)
     }
     @Test
     fun part1() {
-        assertThat(Day15(mutableListOf(17,1,3,16,19,0)).part1()).isEqualTo(694)
+        assertThat(Day15Map(listOf(17,1,3,16,19,0)).part1()).isEqualTo(694)
     }
 
     @Test
     fun part2() {
-        assertThat(Day15Map(mutableListOf(17,1,3,16,19,0)).part2()).isEqualTo(694)
+        assertThat(Day15Map(listOf(17,1,3,16,19,0)).part2()).isEqualTo(21768614)
     }
 }
