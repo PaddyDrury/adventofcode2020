6b285d9ac004de1ca78e20be4934c73092f5618a
Paddy Drury <paddy.e.drury@gmail.com>
2020-12-18 22:10:19 +0000

    refactor day 18

--- src/main/kotlin/day18/Day18.kt
+++ src/main/kotlin/day18/Day18.kt
@@ -12,14 +12,29 @@ class Day18(inputFile: String) {
 abstract class ExpressionEvaluator {
     fun evaluateExpression(expression: String): Long = doEvaluateExpression(expression.replace(" ", ""))
 
-    abstract fun doEvaluateExpression(expression: String): Long
-
-    fun getNextOperand(expression: String, index: Int): Pair<Long, Int> = if (expression[index].isDigit()) Pair(expression[index].toString().toLong(), 1) else evaluateNestedExpression(expression, index)
-
-    private fun evaluateNestedExpression(expression: String, index: Int): Pair<Long, Int> {
-        val nestedExpression = getNestedExpression(expression, index)
-        return Pair(doEvaluateExpression(nestedExpression), nestedExpression.length + 2)
+    private fun doEvaluateExpression(expression: String): Long {
+        val operands = ArrayDeque<Long>()
+        var (firstOperand, index) = getNextOperand(expression, 0)
+        operands.add(firstOperand)
+        while (index < expression.length) {
+            val operator = expression[index++]
+            val (operand, indexDelta) = getNextOperand(expression, index)
+            when (operator) {
+                '*' -> doMultiply(operands, operand)
+                '+' -> doAddition(operands, operand)
+            }
+            index += indexDelta
+        }
+        return doFinalise(operands)
     }
+
+    abstract fun doFinalise(operands: ArrayDeque<Long>): Long
+    abstract fun doAddition(operands: ArrayDeque<Long>, operand: Long)
+    abstract fun doMultiply(operands: ArrayDeque<Long>, operand: Long)
+
+    private fun getNextOperand(expression: String, index: Int): Pair<Long, Int> = if (expression[index].isDigit()) Pair(expression[index].toString().toLong(), 1) else evaluateNestedExpression(expression, index)
+
+    private fun evaluateNestedExpression(expression: String, index: Int): Pair<Long, Int> = getNestedExpression(expression, index).let { Pair(doEvaluateExpression(it), it.length + 2) }
 
     private fun getNestedExpression(expression: String, index: Int): String {
         var unbalancedParentheses = 0
@@ -35,35 +50,13 @@ abstract class ExpressionEvaluator {
 }
 
 class LeftToRightOperatorPrecedenceExpressionEvaluator : ExpressionEvaluator() {
-    override fun doEvaluateExpression(expression: String): Long {
-        var (total, index) = getNextOperand(expression, 0)
-        while (index < expression.length) {
-            val operator = expression[index++]
-            val (operand, indexDelta) = getNextOperand(expression, index)
-            when (operator) {
-                '*' -> total *= operand
-                '+' -> total += operand
-            }
-            index += indexDelta
-        }
-        return total
-    }
+    override fun doFinalise(deque: ArrayDeque<Long>): Long = deque.first()
+    override fun doAddition(deque: ArrayDeque<Long>, operand: Long) { deque.add(operand + deque.removeLast()) }
+    override fun doMultiply(deque: ArrayDeque<Long>, operand: Long) { deque.add(operand * deque.removeLast()) }
 }
 
 class PlusOperatorPrecedenceExpressionEvaluator : ExpressionEvaluator() {
-    override fun doEvaluateExpression(expression: String): Long {
-        val deque = ArrayDeque<Long>()
-        var (firstOperand, index) = getNextOperand(expression, 0)
-        deque.add(firstOperand)
-        while (index < expression.length) {
-            val operator = expression[index++]
-            val (operand, indexDelta) = getNextOperand(expression, index)
-            when (operator) {
-                '*' -> deque.addLast(operand)
-                '+' -> deque.add(operand + deque.removeLast())
-            }
-            index += indexDelta
-        }
-        return deque.fold(1) { acc, l -> acc * l }
-    }
+    override fun doFinalise(deque: ArrayDeque<Long>): Long = deque.fold(1) { acc, l -> acc * l }
+    override fun doAddition(deque: ArrayDeque<Long>, operand: Long) { deque.add(operand + deque.removeLast()) }
+    override fun doMultiply(deque: ArrayDeque<Long>, operand: Long) = deque.addLast(operand)
 }
