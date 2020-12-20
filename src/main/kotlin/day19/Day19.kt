b0c21f648ba9057d7504c8fa47a85ef0a5189f9a
Paddy Drury <paddy.e.drury@gmail.com>
2020-12-20 10:29:54 +0000

    tidy day 19

--- src/main/kotlin/day19/Day19.kt
+++ src/main/kotlin/day19/Day19.kt
@@ -6,32 +6,34 @@ import util.splitWhen
 class Day19(inputFile: String) {
     private val lines: List<String> = readFile(inputFile)
     private val messages = lines.splitWhen(String::isBlank).last()
-    private val rules = toRegularExpressionMap(lines.splitWhen(String::isBlank).first())
+    private val rules = lines.splitWhen(String::isBlank).first()
+        .map { it.split(": ") }
+        .associate { Pair(it.first().toInt(), it.last()) }
+        .let { raw ->
+            val regexCache = mutableMapOf<Int, String>()
+            raw.mapValues { v -> "^(${parseRule(v.key, raw, regexCache, mutableMapOf())})$".toRegex() }
+        }
 
     fun countMatchesOfRule0(): Int = messages.count { rules[0]!!.matches(it) }
 
-    private fun toRegularExpressionMap(rawRules: List<String>): Map<Int, Regex> {
-        val regexMap = mutableMapOf<Int, String>()
-        val rawRuleMap = rawRules.map { it.split(": ") }.associate { Pair(it.first().toInt(), it.last()) }
-
-        return rawRuleMap.mapValues { "^(${parseRule(it.key, it.value, rawRuleMap, regexMap, mutableMapOf())})$".toRegex() }
-    }
-
-    private fun parseRule(ruleNum: Int, rawRule: String, rawRuleMap: Map<Int, String>, regexMap: MutableMap<Int, String>, recursionDepthMap: MutableMap<Int, Int>): String {
-        if(regexMap.contains(ruleNum)) {
-            return regexMap[ruleNum]!!
-        }
-        if(recursionDepthMap.compute(ruleNum) { _, v -> (v ?: 0) + 1 }!! > messages.maxOf(String::length)) return ""
-        val rule = when(rawRule) {
+    private fun parseRule(
+        ruleNum: Int,
+        raw: Map<Int, String>,
+        regexCache: MutableMap<Int, String>,
+        depthCache: MutableMap<Int, Int>
+    ): String {
+        if (regexCache.contains(ruleNum)) return regexCache[ruleNum]!!
+        if (depthCache.compute(ruleNum) { _, v -> (v ?: 0) + 1 }!! >= messages.maxOf(String::length)) return ""
+        val rule = when (raw[ruleNum]!!) {
             "\"a\"" -> "a"
             "\"b\"" -> "b"
-            else -> rawRule.split(" ").map {
-                when(val nestedRuleNum = it.toIntOrNull()) {
+            else -> raw[ruleNum]!!.split(" ").map {
+                when (val nestedRuleNum = it.toIntOrNull()) {
                     null -> it
-                    else -> parseRule(nestedRuleNum, rawRuleMap[nestedRuleNum]!!, rawRuleMap, regexMap, recursionDepthMap)
+                    else -> parseRule(nestedRuleNum, raw, regexCache, depthCache)
                 }
             }.joinToString("")
         }
-        return (if(rule.contains("|")) "($rule)" else rule ).also { regexMap[ruleNum] = it }
+        return (if (rule.contains("|")) "($rule)" else rule).also { regexCache[ruleNum] = it }
     }
 }
