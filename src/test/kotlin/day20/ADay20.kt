1b8ca4f97de2b350790f931d89d6e05a2e5b1b74
Paddy Drury <paddy.e.drury@gmail.com>
2021-01-06 09:26:31 +0000

    fix day 20

--- src/test/kotlin/day20/ADay20.kt
+++ src/test/kotlin/day20/ADay20.kt
@@ -3,15 +3,15 @@ package day20
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
-//class ADay20 {
-//
-//    @Test
-//    fun testPart1() {
-//        assertThat(Day20("inputFiles/day20/input.txt").part1()).isEqualTo(14986175499719L)
-//    }
-//
-//    @Test
-//    fun testPart2() {
-//        assertThat(Day20("inputFiles/day20/input.txt").part2()).isEqualTo(1)
-//    }
-//}
+class ADay20 {
+
+    @Test
+    fun testPart1() {
+        assertThat(Day20("inputFiles/day20/input.txt").part1()).isEqualTo(14986175499719L)
+    }
+
+    @Test
+    fun testPart2() {
+        assertThat(Day20("inputFiles/day20/input.txt").part2()).isEqualTo(2161)
+    }
+}
